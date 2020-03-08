cf8cadc7d603ed384aa55b44afcb4f620610740c
manikanta <mm989@njit.edu>
2020-03-08 12:19:31 -0400

    React Component in separate file

---
+++ src/Components/MyInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function MyInfo(){
+    return (
+        <div>
+            <h1>Mani</h1>
+
+        </div>
+    )
+}
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 
 //function components
 //always one element
-function myApp(){
-    return (<ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-    </ul>)
-}
+import MyInfo from "./Components/MyInfo"
+
 
 //only one element
-ReactDOM.render(<myApp />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
