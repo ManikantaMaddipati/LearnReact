39313d7d6787750729bc5750477f0d4881a129a8
manikanta <mm989@njit.edu>
2020-03-07 19:05:07 -0500

    Initialize project using Create React App

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//ReactDOM.render(What do I want to render, where do I want to render)
+ReactDOM.render(<h1>Hello World</h1>,document.getElementById("root"))
+
+
