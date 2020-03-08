cdc10cc75a4a1860b8d7f5dce98bc5cefed1f020
manikanta <mm989@njit.edu>
2020-03-08 11:26:38 -0400

    react components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//function components
+//always one element
+function myApp(){
+    return (<ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>)
+}
+
 //only one element
-ReactDOM.render(<div><h1>hello</h1><p>World</p></div>, document.getElementById('root'));
+ReactDOM.render(<myApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
