15537ea77cbad332c830670b7335603871c8fe1c
manikanta <mm989@njit.edu>
2020-03-13 22:26:06 -0400

    JSX to js {}

--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -4,11 +4,16 @@ import MainContent from "./MainContent";
 import Footer from "./Footer";
 
 function MyInfo(){
+    var fistname="Manikanta"
+    var lastname="Maddipati"
     return (
         // need to return JSX element
+        //this is JSX not HTML
         <div>
           <Header/>
           <MainContent/>
+          //this is JSX : HTML + JS (this from jsx to js we need att {} example : {lastname + fistname} )
+          <h1> hello {lastname + fistname} !</h1>
           <Footer/>
         </div>
     )
